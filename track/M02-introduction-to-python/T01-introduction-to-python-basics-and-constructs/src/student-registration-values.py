4c960e87f832202ec8ae1b48eceb54cb35a2b106
Your Name <you@example.com>
2026-08-04 17:22:41 +0530

    Studentvalues

---
+++ track/M02-introduction-to-python/T01-introduction-to-python-basics-and-constructs/src/student-registration-values.py
@@ -0,0 +1,14 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+age = int(age_text)
+course_fee = float(course_fee_text)
+attempts_text = str(attempts)
+
+print(age)
+print(type(age))
+print(course_fee)
+print(type(course_fee))
+print(attempts_text)
+print(type(attempts_text))
