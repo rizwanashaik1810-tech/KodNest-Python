1f24a7f81cc455a7c80f8ed7139ca06487e8892b
Your Name <you@example.com>
2026-08-07 15:48:07 +0530

    tuples

---
+++ track/M02-introduction-to-python/T01-introduction-to-python-basics-and-constructs/src/create_and_unpack_tuples.py
@@ -0,0 +1,10 @@
+name = input()
+course = input()
+score = input()
+student_record = (name,course,score)
+
+students_name,course_name,student_score = student_record
+
+print(f"Name: {students_name}")
+print(f"Course: {course_name}")
+print(f"Score: {student_score}")
